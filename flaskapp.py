8890957b640949c319fd26408ea97d1668679c98
Christopher Cook <duncan@yelp.com>
2015-07-30 14:54:49 -0700

    front end stuff

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,25 @@
+import random
+
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+
+IDEAS = [
+    "Build a service to fly moons to Norway",
+    "Disrupt Poverty with node.js",
+    "Give Bizowners bats"
+]
 
 @app.route('/')
 def index():
-    return "Hey buddy"
+    return render_template(
+        'index.html'
+    )
 
+@app.route('/get_idea')
+def get_idea():
+    return random.choice(IDEAS)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
