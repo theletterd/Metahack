563ca9fbeab299300cd285f7309c5e245b315e52
Christopher Cook <duncan@yelp.com>
2015-07-30 19:46:20 -0700

    linkify header, add submission confirmation

--- flaskapp.py
+++ flaskapp.py
@@ -18,9 +18,13 @@ generate_engine = generator.HackathonIdeaGenerator("test_ideas.txt")
 @app.route('/', methods=['GET'])
 def index():
     hackathon_idea = generate_engine.get_hackathon_idea()
+    message = None
+    if request.args.get('from_submission'):
+        message = 'Thanks for your contribution!'
     return render_template(
         'index.html',
-        hackathon_idea=hackathon_idea
+        hackathon_idea=hackathon_idea,
+        message=message
     )
 
 @app.route('/get_idea', methods=['GET'])
@@ -32,7 +36,7 @@ def submit_idea():
     if request.method == 'POST':
         idea = request.form['idea']
         generate_engine.add_new_idea(idea)
-        return redirect('/')
+        return redirect('/?from_submission=1')
     else:
         return render_template(
             'submit.html'
--- static/metahack.js
+++ static/metahack.js
@@ -7,4 +7,27 @@ $(document).ready(function() {
             $('.hackathon-idea').text(data);
         });
     });
+
+    $('.message-area').hide();
+    $('.message-area').fadeIn(500).delay(1500).fadeOut(1000);
+
+    var defaultValue = 'Enter your suggestion...';
+    $('textarea.hackathon-idea').val(defaultValue);
+    $('textarea.hackathon-idea').addClass('empty');
+
+    $('textarea.hackathon-idea').focus(function() {
+        $(this).removeClass('empty');
+        if ($(this).val() === defaultValue) {
+            $(this).val('');
+        }
+    });
+
+    $('textarea.hackathon-idea').blur(function() {
+        console.log($(this).val());
+        if ($(this).val() === '') {
+            $(this).val(defaultValue);
+            $(this).addClass('empty');
+        }
+    });
+
 });
